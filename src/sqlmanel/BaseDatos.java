7a6d446ba439400016d38747bd36ce319f492c51
propa <propa@DESKTOP-10ADTBO>
2016-06-19 23:59:02 +0200

    método eliminar creado

--- src/sqlmanel/BaseDatos.java
+++ src/sqlmanel/BaseDatos.java
@@ -62,5 +62,10 @@ public class BaseDatos {
         rs = s.executeQuery(consulta);
         return rs;
     }
+       public static int eliminar(String tabla, String primaryKeyCol, String primaryKeyVal) throws SQLException {
+        s = conexion.createStatement();
+        int n= s.executeUpdate("delete from " + tabla + " where " + primaryKeyCol + "=" + "'" + primaryKeyVal + "';");
+        return n;
+    }
 
 }
